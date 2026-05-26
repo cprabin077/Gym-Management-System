c601e91ec7e7186bf695d2341f80dc7725e602e7
= <prabin.chaudhary3107@gmail.com>
2026-05-26 16:03:12 +0545

    feat: create superuser

--- member/admin.py
+++ member/admin.py
@@ -1,3 +1,85 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Member
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "email",
+        "gender",
+        "blood_group",
+        "is_active",
+        "joined_at",
+    )
+
+    list_filter = (
+        "gender",
+        "blood_group",
+        "is_active",
+        "joined_at",
+    )
+
+    search_fields = (
+        "first_name",
+        "middle_name",
+        "last_name",
+        "phone",
+        "email",
+    )
+
+    ordering = ("-joined_at",)
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        ("Personal Information", {
+            "fields": (
+                "first_name",
+                "middle_name",
+                "last_name",
+                "dob",
+                "gender",
+            )
+        }),
+
+        ("Contact Information", {
+            "fields": (
+                "phone",
+                "email",
+                "emergency_contact",
+                "address",
+            )
+        }),
+
+        ("Health Information", {
+            "fields": (
+                "height_cm",
+                "weight_kg",
+                "blood_group",
+                "medical_conditions",
+            )
+        }),
+
+        ("Membership Details", {
+            "fields": (
+                "is_active",
+                "joined_at",
+            )
+        }),
+
+        ("Timestamps", {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
