73a95931bcc239f3ffd338e53a5a17872f0aa171
= <kushmiprabin123@gmail.com>
2026-06-08 22:16:15 +0545

    fix: url issues

--- transaction/api/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-
-# from transaction.api.views import 
-
-
-# urlpatterns = [
-#     path('', TrainerView.as_view(), name="trainer"),
-#     path('<int:pk>', TrainerUpdateAndDelete.as_view(), name="trainer-update")
-# ]
